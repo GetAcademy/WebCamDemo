1c7ee13517176b72b8f2ca964ec6110ba70ad9f5
GET Academy <terje@kolderup.net>
2019-05-09 11:38:21 +0200

    x

--- WpfApp4/MainWindow.xaml.cs
+++ WpfApp4/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AForge.Video;
+using AForge.Video.DirectShow;
 
 namespace WpfApp4
 {
@@ -20,9 +25,61 @@ namespace WpfApp4
     /// </summary>
     public partial class MainWindow : Window
     {
+        private VideoCaptureDevice _videoSource;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // enumerate video devices
+            var videoDevices = new FilterInfoCollection(
+                FilterCategory.VideoInputDevice);
+            // create video source
+            _videoSource = new VideoCaptureDevice(
+                videoDevices[0].MonikerString);
+            // set NewFrame event handler
+            _videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+            // start the video source
+            _videoSource.Start();
+            // ...
+            // signal to stop
+            //videoSource.SignalToStop();
+            // ...
+
+
+        }
+
+        private void video_NewFrame(object sender,
+            NewFrameEventArgs eventArgs)
+        {
+            _videoSource.SignalToStop();
+            // get new frame
+            var bitmap = eventArgs.Frame;
+            // process the frame
+
+            MyImage.Dispatcher.Invoke(
+                new Action(() => MyImage.Source = GetBitmapImage(bitmap)));
+
+        }
+
+        private static BitmapImage GetBitmapImage(Bitmap bitmap)
+        {
+            BitmapImage bitmapImage;
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+            }
+
+            return bitmapImage;
         }
     }
 }
